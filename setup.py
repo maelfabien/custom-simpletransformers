1f2d97b4197a15be270fcac4a683e07be8ae14f1
Mael Fabien <mael.fabien@anasen.com>
2020-07-10 10:03:52 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="simpletransformers",
+    version="0.43.4",
+    author="Thilina Rajapakse",
+    author_email="chaturangarajapakshe@gmail.com",
+    description="An easy-to-use wrapper library for the Transformers library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ThilinaRajapakse/simpletransformers/",
+    packages=find_packages(),
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy",
+        "requests",
+        "tqdm>=4.47.0",
+        "regex",
+        "transformers>=2.11.0",
+        "scipy",
+        "scikit-learn",
+        "seqeval",
+        "tensorboardx",
+        "pandas",
+        "tokenizers",
+    ],
+)
